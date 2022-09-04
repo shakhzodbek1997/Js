79f18ebba6ee44dff02ae1a8cd882422576a240e
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-09-04 11:15:02 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// function changeH1Text()
+// {
+//     var heading = document.getElementById('heading');
+//     heading.innerHTML = 'New Heading!';
+// }
+
+
+// function changeH1Text()
+// {
+//     var heading = document.getElementById('heading');
+//     heading.textContent = 'New Heading!';
+// }
+
+
+function changeHeader()
+{
+    var header = document.getElementById('header');
+    var btn = document.getElementById('btn');
+    header.innerText = 'New Heading!';
+    btn.textContent = 'Success';
+
+    
+}
